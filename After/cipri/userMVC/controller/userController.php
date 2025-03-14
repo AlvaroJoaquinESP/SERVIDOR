fb01e47b469d27545c8b484cf8b0ecc1a87bc0da
AlvaroJoaquinESP <alvaroalbarracin02@gmail.com>
2025-03-14 10:31:58 +0100

    somthing-about-server

--- After/cipri/userMVC/controller/userController.php
+++ After/cipri/userMVC/controller/userController.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once("../repository/userRepository.php");
-//  chatGPT: require_once(__DIR__ . "/../repository/userRepository.php");
+require_once("repository/userRepository.php");// doesn't work.
+//  chatGPT: require_once("/../repository/userRepository.php");
 class UserController {
     
     // Cargo el login
--- After/cipri/userMVC/index.php
+++ After/cipri/userMVC/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once("./controller/userController.php");
+// require_once("./config/config.php"); correo cipri.
 
 /**
  * $_GET['action'] y $_GET['controller'] son las variables de las rutas del htacces.
